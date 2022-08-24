6111112f82a664f0241d40ce8611086091f7e0ef
Blueflower-S <camargosil@gmail.com>
2022-08-24 10:14:50 -0300

    Avance__BackURL

--- src/main/java/com/portfolio/sc/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/sc/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendfinal26.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
